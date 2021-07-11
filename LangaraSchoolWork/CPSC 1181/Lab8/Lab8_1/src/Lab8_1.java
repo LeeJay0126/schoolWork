73b16dcc75a97330d3f1cabdb428d6f465e86d59
LeeJay0126 <topaz9889@gmail.com>
2021-07-11 14:32:55 -0700

    1181

--- LangaraSchoolWork/CPSC 1181/Lab8/Lab8_1/src/Lab8_1.java
+++ LangaraSchoolWork/CPSC 1181/Lab8/Lab8_1/src/Lab8_1.java
@@ -12,16 +12,19 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 public class Lab8_1 extends Application {
+
     private Circle circle;
     private double circleX;
     private double circleY;
+    private Pane root;
+    private Line line;
+
     @Override
     public void start(Stage primaryStage) {
-        Pane root = new Pane();
-        circle = new Circle(-10,-10,1);// create a dummy line
-        root.getChildren().add(circle);
+        root = new Pane();
 
         root.setOnMousePressed(new MousePressEventHandler());
+        root.setOnMouseMoved(new MouseMoveEventHandler());
         root.setOnMousePressed(new MousePressEventHandler2());
 
         Scene scene = new Scene(root, 300, 400);
@@ -30,22 +33,46 @@ public class Lab8_1 extends Application {
         primaryStage.show();
     }
 
+    private class MouseMoveEventHandler implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent e) {
+            line.setEndX(e.getX());
+            line.setEndY(e.getY());
+
+            double radius = Math.sqrt(Math.pow(Math.abs(circleX - e.getX()),2) + Math.pow(Math.abs(circleY - e.getY()),2));
+            circle.setRadius(radius);
+
+        }
+    }
+
     private class MousePressEventHandler implements EventHandler<MouseEvent> {
         @Override
         public void handle(MouseEvent e) {
-            Circle temp = new Circle(e.getX(),e.getY(),0);
-            Scale scale = new Scale();
-            scale
-            double radius = Math.sqrt(Math.pow(Math.abs(circleX - e.getX()),2) + Math.pow(Math.abs(circleY - e.getY()),2));
-            circle.setRadius(radius);
+
+            Circle circle = new Circle(e.getX(),e.getY(),0);
+            circle.setFill(Color.TRANSPARENT);
+            circle.setStroke(Color.RED);
+
+            line = new Line();
+            line.setStartX(e.getX());
+            line.setEndX(e.getX());
+            line.setStartY(e.getY());
+            line.setEndY(e.getY());
+            line.setStroke(Color.RED);
+
+            root.getChildren().add(circle);
+            root.getChildren().add(line);
         }
     }
 
     private class MousePressEventHandler2 implements EventHandler<MouseEvent> {
         @Override
         public void handle(MouseEvent e) {
-            double radius = Math.sqrt(Math.pow(Math.abs(circleX - e.getX()),2) + Math.pow(Math.abs(circleY - e.getY()),2));
-            circle.setRadius(radius);
+
+            circle.setStroke(Color.BLUE);
+            line.setStroke(Color.TRANSPARENT);
+
         }
     }
 
