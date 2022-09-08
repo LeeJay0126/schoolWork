f8878f1f51ff01eb0ae6dd6849d316ed9da7747d
LeeJay0126 <topaz9889@gmail.com>
2022-09-08 14:48:48 -0700

    Lab1 CPSC 2030 done

--- LangaraSchoolWork/CPSC 2030/Lab 1/script.js
+++
@@ -1,7 +0,0 @@
-const updatedDate = document.lastModified;
-
-const footerDate = () => {
-    const displayDiv = document.getElementById("secondLine");
-    displayDiv.innerHTML =  'Last Updated on '+updatedDate;
-};
-
