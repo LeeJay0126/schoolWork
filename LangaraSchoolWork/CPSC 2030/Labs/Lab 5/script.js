375d1d836471ead86b6bb293672a884c426b438a
Jay Seung Yeon Lee <79389951+LeeJay0126@users.noreply.github.com>
2022-10-05 16:07:40 -0700

    CPSC 2030 Lab 5 done

--- LangaraSchoolWork/CPSC 2030/Labs/Lab 5/script.js
+++ LangaraSchoolWork/CPSC 2030/Labs/Lab 5/script.js
@@ -13,5 +13,23 @@ function handleInfoToggle() {
 }
 
 function populateImages(subject){
-    
+    photoDiv.innerHTML = "";
+
+    for(let i = 0; i < 13; i++){
+        const height = Math.ceil(500 + Math.random()*200);
+        photoDiv.innerHTML += `<img src="https://source.unsplash.com/440x${height}/?${subject}" alt ="${subject}"></img>`;
+    }
+
+}
+
+function subjectButtonClickHandler(e){
+    const target = e.currentTarget.textContent;
+    populateImages(target);
+}
+
+const buttonContainer = document.querySelector("#subjects");
+const buttons = buttonContainer.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click",subjectButtonClickHandler);
 }
