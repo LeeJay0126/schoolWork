010e465b9d003f8eb57e7a6bdd17414bdca09c0b
LeeJay0126 <topaz9889@gmail.com>
2022-10-21 17:42:24 -0700

    .

---
+++ LangaraSchoolWork/CPSC 2030/Labs/Lab 6/Lab6StarterFiles/lab6/lab6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html style="">
+  <head>
+    <meta charset="utf-8">
+    <title>Lab6: Hello PHP World!</title>
+
+  </head>
+  <body>
+
+    <header>
+        <h1>Hello PHP World!</h1>
+        <div>
+          An Introduction to Server-Side Scripting.
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+
+  </body>
+</html>
