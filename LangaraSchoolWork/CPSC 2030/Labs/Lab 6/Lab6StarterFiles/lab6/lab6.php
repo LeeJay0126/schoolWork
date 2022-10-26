f06a0e09198e40cf2c16f8530354579c9488ae1a
LeeJay0126 <topaz9889@gmail.com>
2022-10-25 17:41:27 -0700

    .

--- LangaraSchoolWork/CPSC 2030/Labs/Lab 6/Lab6StarterFiles/lab6/lab6.php
+++ LangaraSchoolWork/CPSC 2030/Labs/Lab 6/Lab6StarterFiles/lab6/lab6.php
@@ -1,22 +1,79 @@
+<?php
+$myName = "Jay Lee";
+$description = "My name is Jay Lee and I'm 24 years old. I love MMA and I train jiujitsu";
+$favorite_animals = ["cow", "pig", "chicken", "goat", "dog", "cat", "lion"];
+
+function the_developer_profile()
+{
+  global $myName, $description, $favorite_animals;
+  echo
+    '<div>
+      <h2>Developer Profile ' . $myName . '</h2>
+    </div>';
+  echo 
+    '<div>
+      Desciption: ' .$description. '
+    </div>' ;
+  $string = "My favorite animals are ";
+  for($i = 0; $i < count($favorite_animals); $i+=1){
+    if($i == count($favorite_animals)-1){
+      $string .= "and " . $favorite_animals[$i] . ".";
+    }else{
+      $string .= $favorite_animals[$i] .  " ";
+    }
+  };
+  echo 
+    '<div>
+      <p>'.$string.'</p>
+    </div>';
+};
+
+function the_color_form(){
+  echo '
+  <div>
+    <form action="lab6.php" method="post">
+      <p>Which color do you prefer for the text of this page?</p>
+      <input type="color" name="color"/>
+      <input type="submit" value="Submit"></input>
+    </form>
+  </div>
+  ';
+};
+
+function get_color(){
+  if(isset($_POST["color"])){
+    return $_POST["color"];
+  }else{
+    return "#000";
+  }
+};
+
+?>
 <!DOCTYPE html>
-<html style="">
-  <head>
-    <meta charset="utf-8">
-    <title>Lab6: Hello PHP World!</title>
+<html style="color: <?php echo get_color(); ?> ;">
 
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title>Lab6: Hello PHP World!</title>
 
-    <header>
-        <h1>Hello PHP World!</h1>
-        <div>
-          An Introduction to Server-Side Scripting.
-        </div>
-    </header>
+</head>
 
-    <main>
+<body>
 
-    </main>
+  <header>
+    <h1>Hello PHP World!</h1>
+    <div>
+      An Introduction to Server-Side Scripting.
+    </div>
+  </header>
 
-  </body>
+  <main>
+    <?php
+    the_developer_profile();
+    the_color_form();
+    ?>
+  </main>
+
+</body>
+
 </html>
