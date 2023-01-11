90e8bb3093e614e089c2ca58f10cddce42158ac6
LeeJay0126 <topaz9889@gmail.com>
2023-01-11 15:33:27 -0800

    .

--- LangaraSchoolWork/CPSC 2150/A1/assgnFcts/fcts.cpp
+++ LangaraSchoolWork/CPSC 2150/A1/assgnFcts/fcts.cpp
@@ -5,54 +5,79 @@
 #include "fcts.h"
 #include <iostream>
 #include <string>
-#include <climits>  // CHAR_BIT
+#include <climits> // CHAR_BIT
 
 using std::string, std::cerr, std::ostream;
 
 //---------------------------------------------------------------------------//
 //
 //
-// precondition: 
-//    0 <= n <= declared size of A 
+// precondition:
+//    0 <= n <= declared size of A
 // postcondition
 //    A[0] > A[1] > ... > A[n-1]
-bool isStrictlyDescending(const int A[], int n) {
-   cerr << "********************* isStrictlyDescending not implemented *****\n";
-}
+bool isStrictlyDescending(const int A[], int n)
+{
+   if (n - 1 == 0 & A[n] > A[n - 1])
+   {
+      return true;
+   };
+   if (n - 1 == 0 & A[n] < A[n - 1])
+   {
+      return false;
+   }
+   if (A[n - 2] > A[n - 1])
+   {
+      return false;
+   };
 
+   return isStrictlyDescending(A, n - 1);
+}
 
 //---------------------------------------------------------------------------//
 //
 // precondition: n > 0
-void swapPairsLeftToRight(int A[], int n) {
-   cerr << "///////// swapPairsLeftToRight not implemented /////////////////\n";
+void swapPairsLeftToRight(int A[], int n)
+{
+   if (!(n <= 0))
+   {
+      int temp;
+      temp = A[n - 1];
+      A[n - 1] = A[n - 2];
+      A[n - 2] = temp;
+
+      swapPairsLeftToRight(A, n - 2);
+   }
 }
 
 //------------------------------------------------------------------------------
 //
 //
-void outputAsHex(unsigned int n, ostream& out) {
+void outputAsHex(unsigned int n, ostream &out)
+{
    cerr << "+++++++++++++ outputAsHex not implemented ++++++++++++++++++++++\n";
 }
 
 //------------------------------------------------------------------------------
 //
 //
-void outInOctal(unsigned int n, ostream& out) {
+void outInOctal(unsigned int n, ostream &out)
+{
    cerr << "000000 outInOctal not implemented 000000000000000000000000000000\n";
 }
 
 //------------------------------------------------------------------------------
 //
 //
-bool insideInOrder(const string& pattern, int m, const string& str) {
+bool insideInOrder(const string &pattern, int m, const string &str)
+{
    cerr << "-------- insideInOrder not implemented -------------------------\n";
 }
-
 
 //------------------------------------------------------------------------------
 //
 //
-bool insideInOrderI(const string& pattern, int m, const string& str) {
-   cerr << "________ insideInOrder not implemented _________________________\n"; 
+bool insideInOrderI(const string &pattern, int m, const string &str)
+{
+   cerr << "________ insideInOrder not implemented _________________________\n";
 }
